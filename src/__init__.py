0b55d2a61df16e10edae01ba924982d14912362f
nhongomediator-blip <nhongomediator@gmail.com>
2025-11-02 00:29:00 +0200

    added comprehensive inline comments to package init

--- src/__init__.py
+++ src/__init__.py
@@ -2,22 +2,22 @@
 Heart Disease Prediction ML Pipeline
 """
 
-_version_ = "1.0.0"
-_author_ = "Mediator Nhongo and Mercy Ngwenya"
-_description_ = "Machine Learning pipeline for heart disease prediction"
+_version_ = "1.0.0"  # Version number of the ML pipeline
+_author_ = "Mediator Nhongo and Mercy Ngwenya"  # Authors of the pipeline
+_description_ = "Machine Learning pipeline for heart disease prediction"  # Description of the package
 
 # Core components - ONLY import what actually exists
-from .data_loader import DataLoader, MedicalDataLoader
-from .preprocessor import DataPreprocessor
-from .model_factory import ModelFactory
-from .trainer import ModelTrainer, MedicalModelTrainer, BaseTrainer
+from .data_loader import DataLoader, MedicalDataLoader  # Import data loading classes
+from .preprocessor import DataPreprocessor  # Import data preprocessing class
+from .model_factory import ModelFactory  # Import model creation and training class
+from .trainer import ModelTrainer, MedicalModelTrainer, BaseTrainer  # Import training classes
 
-_all_ = [
-    'DataLoader',
-    'MedicalDataLoader', 
-    'DataPreprocessor',
-    'ModelFactory',
-    'ModelTrainer', 
-    'MedicalModelTrainer',
-    'BaseTrainer',
+_all_ = [  # List of public classes and functions exposed by this package
+    'DataLoader',  # Main data loader class
+    'MedicalDataLoader',  # Specialized medical data loader
+    'DataPreprocessor',  # Data preprocessing class
+    'ModelFactory',  # Model creation and training factory
+    'ModelTrainer',  # Main model trainer class
+    'MedicalModelTrainer',  # Specialized medical model trainer
+    'BaseTrainer',  # Abstract base trainer class
 ]
